0f596269e7452cb969f2039d87f1bda4ea132b01
sapho-sys <saphonkunzi@gmail.com>
2022-07-18 16:15:01 +0200

    almost!

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 
@@ -6,6 +7,18 @@ var app = express();
 app.get('/hello', function(res, req){
     res.send("Hello World!")
 })
+
+//this is a GET route
+app.get('/products/:id', function(){
+    console.log(req.params.id);
+    res.send("you sent me :" + req.params.id);
+
+});
+
+//this is a POST route
+app.post('/hello', function(){
+
+});
 //start the server
 var server = app.listen(3000, function(){
     
