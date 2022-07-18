ebd211168482346b6258fefbe93d56af423707cc
sapho-sys <saphonkunzi@gmail.com>
2022-07-18 14:37:37 +0200

    learning

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+
+app.get('/hello', function(res, req){
+    res.send("Hello World!")
+})
+//start the server
+var server = app.listen(3000, function(){
+    
+
+    var host = server.address().address;
+    var port = server.address().port;
+   
+    console.log('Example app listening at http://%s:%s', host, port);
+   
+   
+});
